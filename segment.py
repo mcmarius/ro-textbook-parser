e478cc09096720aa88c6d5c3b843e30e51e9093c
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-01-24 13:54:42 +0200

    Done with 6th grade, conditional sort, more checks

--- segment.py
+++ segment.py
@@ -4,19 +4,37 @@ import pymupdf
 
 MIN_TOC_PAGE = 3
 MAX_TOC_PAGE = 10
+MIN_SEC_PER_PAGE = 15
 
-pdf1 = pymupdf.open('data/Manual_Cls 8_Lb Ro_3_Corint.pdf')
-pdf2 = pymupdf.open('data/Manual_Cls 8_Lb Ro_1_ArtKlett.pdf')
-pdf3 = pymupdf.open('data/Manual_Cls 8_Lb Ro_2_Artemis.pdf')
-pdf4 = pymupdf.open('data/Manual_Cls 6_Lb Ro_1_EDP.pdf')
-pdf5 = pymupdf.open('data/Manual_Cls 6_Lb Ro_3_Litera.pdf')
-pdf6 = pymupdf.open('data/Manual_Cls 6_Lb Ro_4_CD Press.pdf')
-pdf7 = pymupdf.open('data/Manual_Cls 6_Lb Ro_5_Booklet.pdf')
+pdf01 = pymupdf.open('data/Manual_Cls 8_Lb Ro_3_Corint.pdf')
+pdf02 = pymupdf.open('data/Manual_Cls 8_Lb Ro_1_ArtKlett.pdf')
+pdf03 = pymupdf.open('data/Manual_Cls 8_Lb Ro_2_Artemis.pdf')
+pdf04 = pymupdf.open('data/Manual_Cls 6_Lb Ro_1_EDP.pdf')
+pdf05 = pymupdf.open('data/Manual_Cls 6_Lb Ro_3_Litera.pdf')
+pdf06 = pymupdf.open('data/Manual_Cls 6_Lb Ro_4_CD Press.pdf')
+pdf07 = pymupdf.open('data/Manual_Cls 6_Lb Ro_5_Booklet.pdf')
+pdf08 = pymupdf.open('data/Manual_Cls 6_Lb Ro_7_Paralela 45.pdf')
+pdf09 = pymupdf.open('data/Manual_Cls 6_Lb Ro_8_Intuitext.pdf')
+pdf10 = pymupdf.open('data/Manual_Cls 6_Lb Ro_6_Corint.pdf')
+pdf11 = pymupdf.open('data/Manual_Cls 6_Lb Ro_2_ArtKlett.pdf')
 # page_lines = pdf.get_page_text(11).split('\n')
+
+SORT_NEEDS = {
+    pdf01.name: False, pdf02.name:  True, pdf03.name: False,
+    pdf04.name: False, pdf05.name:  True, pdf06.name: False,
+    pdf07.name:  True, pdf08.name: False, pdf09.name: False,
+    pdf10.name: False, pdf11.name: False
+}
+
+def safe_int(nr):
+    try:
+        return int(nr)
+    except ValueError:
+        return None
 
 
 def is_toc_page(pdf_page):
-    return len([x for x in re.split('(\d{1,}[^\.][– ]?[\d]?)\n', pdf_page) if x and x[0].isdigit()]) > 20
+    return len([x for x in re.split('(\d{1,}[^\.][– ]?[\d]?)\n', pdf_page) if x and x[0].isdigit()]) >= MIN_SEC_PER_PAGE and pdf_page.count(';') < 20
 
 
 def find_chapter(pdf):
@@ -25,8 +43,9 @@ def find_chapter(pdf):
         if is_toc_page(pdf.get_page_text(i)):
             fixed_lines = fix_lines(pdf.get_page_text(i).split('\n'))
             to_skip = 0
+            # print(fixed_lines)
             for i, line in enumerate(fixed_lines):
-                if 'UNITATEA' in line or 'Unitatea' in line:
+                if 'UNITATEA' in line or 'Unitatea' in line or re.findall('[2-8]\t', line):
                     # print(f'got {i} -> {line}')
                     if '.  . ' in line[-10:]:
                         sep = '\.  \. '
@@ -38,8 +57,12 @@ def find_chapter(pdf):
                         sep = '/'
                     else:
                         sep = ' '
+                    if 'DE ÎNVĂȚĂRE' in line:
+                        line_lim = 22
+                    else:
+                        line_lim = 11
                     # page_in_line = line.rsplit(sep, 1)[1].strip()
-                    candidates = re.findall(f'{sep}\s*(\d+)', line[22:])
+                    candidates = re.findall(f'{sep}\s*(\d+)', line[line_lim:])
                     if len(candidates) >= 1:
                         page_in_line = candidates[0].strip()
                         if page_in_line[0].isdigit():
@@ -67,8 +90,14 @@ def get_toc_pages(lines):
     page_lines = [small_line for line in page_lines for small_line in line.split('\n') if small_line]
     if len(page_lines) < 10:  # only whitespace is the separator; fortunately, no numbers in titles
         return [num for line in lines.split('\n') for num in re.findall('(\d{2,})', line)]
-    # print(page_lines)
+    #print(lines)
+    #print('-----------------\n\n')
+    #print(page_lines)
     for line in page_lines:
+        if safe_int(line):
+            pages.append(line.strip())
+            #print(f'got {line}')
+            continue
         sep = '..'
         if ' .   .' in line:
             sep = ' .   .'
@@ -99,8 +128,12 @@ def get_toc_pages(lines):
                 #print(f"got >> {''.join(x for x in page_in_line if x.isdigit())}")
                 if '.' in page_in_line[0:3]:
                     continue
-                pages.append(''.join(x for x in page_in_line if x.isdigit()))
+                if '-' in page_in_line:
+                    pages += page_in_line.split('-')
+                else:
+                    pages.append(''.join(x for x in page_in_line if x.isdigit()))
     # print(page_lines)
+    # print(len(pages))
     # fix_lines([z for x in pdf4.get_page_text(5, sort=True).split('\n') for z in x.split('  ') if z and '/' in z])
     # for x in re.split('[^L] ?/? ?(\d{2,3})', pdf_page):
     # for x in re.split('[^L] ?/? ?(\d{2,3})', lines):
@@ -117,10 +150,10 @@ def find_toc(pdf):
     toc_pages = []
     for i in range(MIN_TOC_PAGE, MAX_TOC_PAGE):
         if is_toc_page(pdf.get_page_text(i)):
-            toc_page_list = get_toc_pages(pdf.get_page_text(i, sort=True))
+            toc_page_list = get_toc_pages(pdf.get_page_text(i, sort=SORT_NEEDS[pdf.name]))
             # fixed_lines = fix_lines(pdf.get_page_text(i).split('\n'))
             # toc_page_list = get_toc_pages(fixed_lines) # get_toc_pages(pdf.get_page_text(i))
-            if len(toc_page_list) > 20:
+            if len(toc_page_list) >= MIN_SEC_PER_PAGE:
                 toc_pages += toc_page_list
     return sorted(list(set([int(x) for x in toc_pages if len(x) > 1 and len(x) < 4 and int(x) < len(pdf)])))
 
@@ -150,20 +183,28 @@ def fix_lines(lines):
 
 
 def chapter_tests():
-    assert(find_chapter(pdf1) == [16, 44, 78, 112, 150, 184])
-    assert(find_chapter(pdf2) == [10, 50, 92, 126, 158])
-    assert(find_chapter(pdf3) == [18, 38, 70, 96, 122, 152])
-    assert(find_chapter(pdf4) == [13, 39, 63, 97, 137, 169])
-    assert(find_chapter(pdf5) == [11, 51, 83, 113, 141, 167])
-    assert(find_chapter(pdf6) == [20, 52, 84, 112, 154])
-    assert(find_chapter(pdf7) == [11, 57, 103, 143, 179])
+    assert(find_chapter(pdf01) == [16, 44, 78, 112, 150, 184])
+    assert(find_chapter(pdf02) == [1, 10, 50, 92, 126, 158])
+    assert(find_chapter(pdf03) == [18, 38, 70, 96, 122, 152])
+    assert(find_chapter(pdf04) == [13, 39, 63, 97, 137, 169])
+    assert(find_chapter(pdf05) == [11, 51, 83, 113, 141, 167])
+    assert(find_chapter(pdf06) == [20, 52, 84, 112, 154])
+    assert(find_chapter(pdf07) == [11, 57, 103, 143, 179])
+    assert(find_chapter(pdf08) == [13, 49, 93, 127, 159, 185])
+    assert(find_chapter(pdf09) == [14, 45, 66, 108, 146, 181, 213])  # good enough
+    assert(find_chapter(pdf10) == [12, 48, 88, 131, 164, 192])
+    assert(find_chapter(pdf11) == [10, 44, 84, 122, 154])
 
 def toc_tests():
-    assert(find_toc(pdf1) == [14, 16, 22, 24, 27, 29, 31, 34, 35, 36, 37, 38, 39, 41, 42, 44, 53, 55, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 81, 83, 84, 86, 89, 92, 94, 96, 97, 99, 101, 103, 105, 106, 108, 110, 112, 117, 118, 120, 124, 127, 128, 130, 133, 136, 138, 139, 141, 142, 143, 144, 146, 148, 150, 156, 157, 161, 163, 165, 168, 169, 172, 173, 176, 177, 180, 182, 184, 189, 195, 197, 199, 201, 203, 205, 207, 209, 211, 214, 218, 220, 221, 224])
-    assert(find_toc(pdf2) == [10, 15, 17, 20, 22, 23, 25, 26, 28, 31, 34, 36, 38, 40, 42, 44, 48, 50, 56, 58, 61, 63, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 90, 92, 94, 95, 96, 98, 100, 101, 103, 104, 105, 107, 109, 111, 113, 115, 118, 120, 124, 126, 130, 132, 138, 140, 141, 143, 144, 146, 147, 149, 150, 152, 156, 158, 164, 166, 168, 170, 172, 174, 176, 179, 181, 182, 184, 185, 187, 188, 190, 191, 193, 194, 196, 198, 200, 204, 205, 207])
-    assert(find_toc(pdf3) == [13, 18, 19, 21, 22, 23, 27, 29, 31, 33, 35, 38, 39, 48, 49, 54, 57, 59, 61, 63, 66, 70, 71, 72, 77, 79, 81, 85, 87, 91, 92, 96, 97, 101, 102, 104, 107, 109, 112, 114, 117, 119, 122, 124, 128, 129, 131, 133, 136, 139, 144, 148, 152, 154, 161, 163, 166, 170, 175, 177, 181, 184, 187, 189, 190, 192])
-    assert(find_toc(pdf4) == [11, 13, 14, 16, 22, 24, 26, 30, 33, 35, 37, 39, 40, 42, 44, 46, 49, 51, 54, 57, 60, 62, 63, 64, 66, 69, 71, 73, 76, 80, 83, 84, 88, 93, 95, 97, 98, 100, 106, 108, 110, 112, 116, 117, 119, 123, 128, 131, 135, 137, 138, 140, 144, 149, 152, 157, 158, 161, 164, 167, 169, 170, 172, 178, 181, 184, 186, 188, 189, 191, 192, 193, 196, 200, 202, 205, 207])
-    assert(find_toc(pdf5) == [11, 12, 15, 16, 18, 21, 24, 26, 28, 31, 34, 35, 37, 39, 40, 45, 47, 49, 51, 52, 55, 57, 58, 60, 63, 65, 66, 68, 70, 71, 73, 75, 78, 80, 82, 83, 84, 85, 86, 87, 88, 90, 91, 93, 94, 95, 96, 99, 100, 103, 104, 106, 108, 109, 110, 112, 113, 114, 115, 116, 118, 120, 121, 123, 125, 127, 128, 130, 133, 135, 137, 139, 140, 141, 142, 144, 145, 146, 149, 151, 152, 154, 157, 160, 162, 164, 166, 167, 168, 169, 171, 173, 175, 178, 180, 182, 184, 185, 190, 192])
-    assert(find_toc(pdf6) == [14, 20, 22, 24, 26, 27, 30, 32, 40, 44, 52, 54, 56, 58, 60, 62, 64, 68, 76, 80, 84, 88, 90, 92, 93, 95, 96, 98, 100, 102, 106, 112, 114, 116, 118, 122, 124, 126, 128, 131, 133, 136, 139, 140, 144, 154, 156, 158, 160, 162, 168, 169, 171, 176, 178, 180, 184, 188])
-    assert(find_toc(pdf7) == [11, 12, 24, 28, 33, 35, 38, 40, 43, 46, 49, 53, 55, 57, 58, 70, 74, 77, 83, 86, 89, 91, 93, 96, 98, 101, 103, 104, 110, 113, 117, 119, 123, 124, 126, 127, 131, 133, 136, 138, 141, 143, 144, 156, 160, 162, 167, 169, 171, 174, 176, 178, 179, 180, 187, 192, 195, 199, 203, 205, 210, 212, 214, 216, 219, 220, 221, 222, 223, 224])
+    assert(find_toc(pdf01) == [14, 16, 22, 24, 27, 29, 31, 34, 35, 36, 37, 38, 39, 41, 42, 44, 53, 55, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 81, 83, 84, 86, 89, 92, 94, 96, 97, 99, 101, 103, 105, 106, 108, 110, 112, 117, 118, 120, 124, 127, 128, 130, 133, 136, 138, 139, 141, 142, 143, 144, 146, 148, 150, 156, 157, 161, 163, 165, 168, 169, 172, 173, 176, 177, 180, 182, 184, 189, 195, 197, 199, 201, 203, 205, 207, 209, 211, 214, 218, 220, 221, 224])
+    assert(find_toc(pdf02) == [10, 15, 17, 20, 22, 23, 25, 26, 28, 31, 34, 36, 38, 40, 42, 44, 48, 50, 56, 58, 61, 63, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 90, 92, 94, 95, 96, 98, 100, 101, 103, 104, 105, 107, 109, 111, 113, 115, 118, 120, 124, 126, 130, 132, 138, 140, 141, 143, 144, 146, 147, 149, 150, 152, 156, 158, 164, 166, 168, 170, 172, 174, 176, 179, 181, 182, 184, 185, 187, 188, 190, 191, 193, 194, 196, 198, 200, 204, 205, 207])
+    assert(find_toc(pdf03) == [13, 18, 19, 21, 22, 23, 27, 29, 31, 33, 35, 38, 39, 48, 49, 54, 57, 59, 61, 63, 66, 70, 71, 72, 77, 79, 81, 85, 87, 91, 92, 96, 97, 101, 102, 104, 107, 109, 112, 114, 117, 119, 122, 124, 128, 129, 131, 133, 136, 139, 144, 148, 152, 154, 161, 163, 166, 170, 175, 177, 181, 184, 187, 189, 190, 192])
+    assert(find_toc(pdf04) == [11, 13, 14, 16, 22, 24, 26, 30, 33, 35, 37, 39, 40, 42, 44, 46, 49, 51, 54, 57, 60, 62, 63, 64, 66, 69, 71, 73, 76, 80, 83, 84, 88, 93, 95, 97, 98, 100, 106, 108, 110, 112, 116, 117, 119, 123, 128, 131, 135, 137, 138, 140, 144, 149, 152, 157, 158, 161, 164, 167, 169, 170, 172, 178, 181, 184, 186, 188, 189, 191, 192, 193, 196, 200, 202, 205, 207])
+    assert(find_toc(pdf05) == [11, 12, 15, 16, 18, 21, 24, 26, 28, 31, 34, 35, 37, 39, 40, 45, 47, 49, 51, 52, 55, 57, 58, 60, 63, 65, 66, 68, 70, 71, 73, 75, 78, 80, 82, 83, 84, 85, 86, 87, 88, 90, 91, 93, 94, 95, 96, 99, 100, 103, 104, 106, 108, 109, 110, 112, 113, 114, 115, 116, 118, 120, 121, 123, 125, 127, 128, 130, 133, 135, 137, 139, 140, 141, 142, 144, 145, 146, 149, 151, 152, 154, 157, 160, 162, 164, 166, 167, 168, 169, 171, 173, 175, 178, 180, 182, 184, 185, 190, 192])
+    assert(find_toc(pdf06) == [14, 20, 22, 24, 26, 27, 30, 32, 40, 44, 52, 54, 56, 58, 60, 62, 64, 68, 76, 80, 84, 88, 90, 92, 93, 95, 96, 98, 100, 102, 106, 112, 114, 116, 118, 122, 124, 126, 128, 131, 133, 136, 139, 140, 144, 154, 156, 158, 160, 162, 168, 169, 171, 176, 178, 180, 184, 188])
+    assert(find_toc(pdf07) == [11, 12, 24, 28, 33, 35, 38, 40, 43, 46, 49, 53, 55, 57, 58, 70, 74, 77, 83, 86, 89, 91, 93, 96, 98, 101, 103, 104, 110, 113, 117, 119, 123, 124, 126, 127, 131, 133, 136, 138, 141, 143, 144, 156, 160, 162, 167, 169, 171, 174, 176, 178, 179, 180, 187, 192, 195, 199, 203, 205, 210, 212, 214, 216, 219, 220, 221, 222, 223, 224])
+    assert(find_toc(pdf08) == [13, 14, 17, 18, 21, 29, 31, 34, 36, 38, 40, 41, 42, 43, 46, 47, 48, 49, 50, 53, 54, 57, 64, 67, 69, 71, 73, 75, 77, 81, 85, 88, 90, 91, 92, 93, 94, 97, 98, 100, 108, 111, 117, 119, 121, 124, 125, 126, 127, 128, 131, 132, 134, 135, 136, 144, 147, 149, 151, 154, 155, 156, 157, 158, 159, 160, 163, 167, 169, 173, 177, 180, 181, 183, 184, 185, 186, 190, 192, 198, 201, 204, 206, 207, 208, 210, 211, 212, 215])
+    assert(find_toc(pdf09) == [10, 11, 13, 14, 19, 21, 24, 26, 28, 29, 31, 33, 35, 37, 38, 39, 40, 43, 45, 46, 49, 51, 53, 55, 56, 58, 59, 60, 61, 64, 66, 67, 70, 74, 76, 78, 81, 83, 88, 90, 92, 93, 94, 97, 99, 100, 105, 107, 108, 116, 118, 119, 121, 123, 126, 128, 131, 133, 134, 136, 137, 139, 143, 145, 146, 149, 151, 153, 155, 158, 162, 164, 166, 168, 171, 173, 175, 176, 179, 181, 182, 186, 188, 189, 191, 194, 195, 196, 198, 200, 201, 202, 204, 206, 207, 210, 212, 213, 215, 218, 220, 221, 223])
+    assert(find_toc(pdf10) == [12, 13, 16, 18, 19, 20, 22, 26, 28, 31, 41, 42, 46, 48, 49, 53, 54, 56, 58, 61, 63, 66, 83, 84, 86, 88, 89, 92, 94, 96, 98, 100, 103, 105, 107, 110, 126, 127, 130, 131, 133, 134, 136, 137, 139, 141, 142, 144, 146, 148, 150, 158, 160, 162, 164, 165, 168, 171, 173, 175, 177, 179, 181, 187, 188, 190, 192, 194, 198, 200, 202, 203, 205, 208, 213, 215, 218, 219, 221, 222])
+    assert(find_toc(pdf11) == [10, 13, 14, 16, 17, 19, 20, 21, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 47, 49, 52, 54, 56, 58, 60, 62, 64, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 91, 92, 95, 96, 98, 100, 102, 105, 106, 109, 112, 113, 115, 116, 118, 120, 122, 124, 126, 128, 129, 132, 134, 136, 140, 142, 144, 146, 148, 150, 152, 154, 158, 160, 162, 164, 166, 168, 170, 172, 175, 176, 178, 180, 182, 184, 186, 188, 190, 192])
 
